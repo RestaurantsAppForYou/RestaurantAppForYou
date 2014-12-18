202e5dca60ea1dbfb872e43185b601e0c4043465
RestaurantsAppForYou <restaurantsappforyou@gmail.com>
2014-12-18 13:22:00 -0500

    Adding Translation in getAllRestaurants Of Controller

--- Restaurant-Web/src/com/restaurant/controller/RestaurantController.java
+++ Restaurant-Web/src/com/restaurant/controller/RestaurantController.java
@@ -1,11 +1,16 @@
 package com.restaurant.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.restaurant.service.RestaurantsServiceInterface;
+import com.restaurants.entity.RestaurantsEntity;
+import com.restaurants.info.Location;
 import com.restaurants.info.Restaurants;
 
 @Controller
@@ -19,7 +24,37 @@ public class RestaurantController {
 	public ModelAndView getRestaurants() {
 		// TODO Auto-generated method stub
 
+		int i = 0;
+		String restaurantId = "" + i;
+		List<Restaurants> restaurantsList = new ArrayList<Restaurants>();
+
 		ModelAndView modelAndView = new ModelAndView("RestaurantsList");
+
+		List<RestaurantsEntity> restaurantsEntityList = restaurantsService
+				.getAllRestaurants();
+
+		for (RestaurantsEntity restaurantsEntity : restaurantsEntityList) {
+
+			Location location = new Location();
+			Restaurants restaurants = new Restaurants();
+			// Location Details Translation
+			location.setCity(restaurantsEntity.getLocation().getCity());
+			location.setContactDetails(restaurantsEntity.getLocation()
+					.getContactDetails());
+			location.setState(restaurantsEntity.getLocation().getState());
+			location.setStreet(restaurantsEntity.getLocation().getStreet());
+			location.setZipCode(restaurantsEntity.getLocation().getZipCode());
+
+			// Restaurant details Translation
+			restaurants.setHoursOfOperation(restaurantsEntity
+					.getHoursOfOperation());
+			restaurants.setLocation(location);
+			restaurants.setName(restaurantsEntity.getName());
+			restaurants.setRating(restaurantsEntity.getRating());
+			i++;
+			restaurants.setRestaurantID(restaurantId);
+
+		}
 
 		return modelAndView;
 	}
